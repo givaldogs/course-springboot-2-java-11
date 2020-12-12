7886bf1afc28b12d10beb2f859485e6bcc162647
givaldogs <givaldowanda@gmail.com>
2020-12-12 19:51:03 -0300

    User Update

--- src/main/java/com/educandoweb/course/services/UserService.java
+++ src/main/java/com/educandoweb/course/services/UserService.java
@@ -14,25 +14,38 @@ public class UserService {
 
 	@Autowired
 	private UserRepository repository;
-	
+
 	public List<User> findAll() {
 		return repository.findAll();
-		
+
 	}
-	
+
 	public User findById(Long id) {
-	//	java.util.Optional<User> obj = repository.findById(id);
+		// java.util.Optional<User> obj = repository.findById(id);
 		Optional<User> obj = repository.findById(id);
 		return obj.get();
-		
+
 	}
-	
-	public User insert (User obj) {
+
+	public User insert(User obj) {
 		return repository.save(obj);
 	}
-	
+
 	public void delete(Long id) {
 		repository.deleteById(id);
-		
+
 	}
+
+	public User update(Long id, User obj) {
+		User entity = repository.getOne(id);
+		updateData(entity, obj);
+		return repository.save(entity);
+	}
+
+	private void updateData(User entity, User obj) {
+		// TODO Auto-generated method stub
+		entity.setName(obj.getName());
+		entity.setEmail(obj.getEmail());
+		entity.setPhone(obj.getPhone());
+		}
 }
